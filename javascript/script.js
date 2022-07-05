9d5e2c9d7cb06ebe421834dc34e40f1095c96d19
Krix12 <>
2022-07-05 14:03:34 +0200

    added display on scroll

--- javascript/script.js
+++ javascript/script.js
@@ -18,6 +18,24 @@ window.addEventListener("click", function(e) {
 menu.style.display = "none";
 });
 
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
 let progress = document.getElementById("scrollbar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function() {
